6e4c49be9b1e419bb7334a9653e26f3873c89a5f
Mark Hamstra <hello@markhamstra.com>
2025-04-23 16:29:39 +0200

    feat(tools): add EditChunk and EditTemplate tools
    Introduce new tools for editing chunks and templates, enabling modification of existing reusable HTML/code and resource-rendering templates in the system.

---
+++ core/components/modai/src/Tools/EditChunk.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace modAI\Tools;
+
+use MODX\Revolution\modChunk;
+use MODX\Revolution\modX;
+
+class EditChunk implements ToolInterface
+{
+    private $modx;
+
+    public static function getSuggestedName(): string
+    {
+        return 'edit_chunk';
+    }
+
+    public static function getDescription(): string
+    {
+        return "Edits an existing Chunk, which is a reusable piece of HTML or other code that can be inserted into pages, templates, or other elements. Use when explicitly asked to create a chunk or when creating templates to break up reusable pieces. Get the current content to edit using the get_chunks tool. Chunks can be used by adding [[\$name_of_chunk]] in a template or elsewhere. ALWAYS ask for explicit user confirmation with the chunk name, description, and category name in a separate message BEFORE calling this function.";
+    }
+
+    public static function getParameters(): array
+    {
+        return [
+            'type' => 'object',
+            'properties' => [
+                'chunk' => [
+                    'type' => 'object',
+                    'properties' => [
+                        'name' => [
+                            'type' => 'string',
+                            "description" => 'Name of the chunk to edit'
+                        ],
+                        'description' => [
+                            'type' => 'string',
+                            "description" => 'Description of the chunk, summary of what the chunk renders'
+                        ],
+                        'content' => [
+                            'type' => 'string',
+                            "description" => 'The updated content of the chunk'
+                        ],
+                    ],
+                    "required" => ["name", "description", 'content'],
+                    "description" => "The chunk to edit",
+                ],
+            ],
+            "required" => ["chunk"],
+        ];
+    }
+
+    public static function getConfig(): array
+    {
+        return [];
+    }
+
+    public function __construct(modX $modx, array $config)
+    {
+        $this->modx = $modx;
+    }
+
+    /**
+     * @param array $parameters
+     * @return string
+     */
+    public function runTool($parameters): string
+    {
+        if (!self::checkPermissions($this->modx)) {
+            return json_encode(['success' => false, "message" => "You do not have permission to use this tool."]);
+        }
+
+        if (empty($parameters)) {
+            return json_encode(['success' => false, 'message' => 'Parameters are required.']);
+        }
+
+        $chunk = $this->modx->getObject(modChunk::class, ['name' => $parameters['chunk']['name']]);
+        if (!$chunk) {
+            return json_encode(['success' => false, 'message' => 'Chunk not found with name.']);
+        }
+        $chunk->set('description', (string)$parameters['chunk']['description']);
+        $chunk->set('snippet', (string)$parameters['chunk']['content']);
+        if ($chunk->save()) {
+            return json_encode(['success' => true, 'message' => 'Chunk updated. Use with: [[$' . $chunk->get('name') . ']]']);
+        }
+
+        return json_encode(['success' => false, 'message' => 'Could not save chunk.']);
+    }
+
+    public static function checkPermissions(modX $modx): bool
+    {
+        return $modx->hasPermission('save_chunk');
+    }
+}
---
+++ core/components/modai/src/Tools/EditTemplate.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace modAI\Tools;
+
+use MODX\Revolution\modTemplate;
+use MODX\Revolution\modX;
+
+class EditTemplate implements ToolInterface
+{
+    private $modx;
+
+    public static function getSuggestedName(): string
+    {
+        return 'edit_template';
+    }
+
+    public static function getDescription(): string
+    {
+        return "Edits an existing template, which is used to render a resource on the front-end of the website. Use when explicitly asked to edit a template. In most cases, you should also check other templates to see the preferred coding style and structure of the website. Get the current content of the template with the get_templates tool. ALWAYS ask for explicit user confirmation with the template name and a summary of the changes you are making BEFORE calling this function.";
+    }
+
+    public static function getParameters(): array
+    {
+        return [
+            'type' => 'object',
+            'properties' => [
+                'template' => [
+                    'type' => 'object',
+                    'properties' => [
+                        'name' => [
+                            'type' => 'string',
+                            "description" => 'Name of the template to edit'
+                        ],
+                        'description' => [
+                            'type' => 'string',
+                            "description" => 'Description of the template'
+                        ],
+                        'content' => [
+                            'type' => 'string',
+                            "description" => 'The updated content of the template'
+                        ],
+                    ],
+                    "required" => ["name", "description", 'content'],
+                    "description" => "The template to edit"
+                ],
+            ],
+            "required" => ["template"]
+        ];
+    }
+
+    public static function getConfig(): array
+    {
+        return [];
+    }
+
+    public function __construct(modX $modx, array $config)
+    {
+        $this->modx = $modx;
+    }
+
+    /**
+     * @param array $parameters
+     * @return string
+     */
+    public function runTool($parameters): string
+    {
+        if (!self::checkPermissions($this->modx)) {
+            return json_encode(['success' => false, "message" => "You do not have permission to use this tool."]);
+        }
+
+        if (empty($parameters)) {
+            return json_encode(['success' => false, 'message' => 'Parameters are required.']);
+        }
+
+        $template = $this->modx->getObject(modTemplate::class, ['templatename' => $parameters['template']['name']]);
+        if (!$template) {
+            return json_encode(['success' => false, 'message' => 'Template not found with name.']);
+        }
+        $template->set('description', (string)$parameters['template']['description']);
+        $template->set('content', (string)$parameters['template']['content']);
+        if ($template->save()) {
+            return json_encode(['success' => true, 'message' => 'Template updated.']);
+        }
+
+        return json_encode(['success' => false, 'message' => 'Could not save template.']);
+    }
+
+    public static function checkPermissions(modX $modx): bool
+    {
+        return $modx->hasPermission('save_template');
+    }
+}
